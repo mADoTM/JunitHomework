f4d5a479ac7763858e45586e1d2823069d1a590a
Максим Долженко <maxim_2003@mail.ru>
2022-10-25 11:29:47 +0300

    Add new module 'models' and remove default main classes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,22 @@ plugins {
     java
 }
 
-group = "ru.mail"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "ru.mail"
+    version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+subprojects {
+    apply(plugin = "java")
+    dependencies {
+        implementation("org.jetbrains:annotations:20.1.0")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    }
 }
 
 tasks.getByName<Test>("test") {
