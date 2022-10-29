c760366958cc2eab8a0e090d78ebff3f89a451c4
Максим Долженко <maxim_2003@mail.ru>
2022-10-29 09:16:06 +0300

    Add getter method for books. Remove field "size". Update method getFirstFreeIndex(). Now it should work about O(logN)

--- library/src/main/java/ru/mail/Library.java
+++ library/src/main/java/ru/mail/Library.java
@@ -3,15 +3,15 @@ package ru.mail;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.TreeSet;
 
 public class Library {
     private final Book[] books;
 
-    private final int size;
+    private final @NotNull TreeSet<Integer> freeIndexes;
 
     public Library(int size,
                    @NotNull List<Book> books) {
-        this.size = size;
         this.books = new Book[size];
 
         if(size < books.size()) {
@@ -21,16 +21,22 @@ public class Library {
         for(int i = 0; i < books.size(); i++) {
             this.books[i] = books.get(i);
         }
+
+        freeIndexes = new TreeSet<>();
+
+        for(int i = books.size(); i < size; i++) {
+            freeIndexes.add(i);
+        }
     }
 
     public int getSize() {
-        return size;
+        return books.length;
     }
 
     public void printBooks() {
         for(int i = 0; i < books.length; i++) {
             try {
-                getBook(i);
+                printBook(i);
             }
             catch(IllegalArgumentException e) {
                 System.out.print(i + "::");
@@ -39,16 +45,19 @@ public class Library {
         }
     }
 
-    public void getBook(int index) {
-        if(index < 0 || index >= size) {
-            throw new IndexOutOfBoundsException();
-        }
-
+    public @NotNull Book getBook(int index) {
         Book book = books[index];
         if(book == null) {
             throw new IllegalArgumentException("There isn't book with such index");
         }
+        return book;
+    }
+
+    public void printBook(int index) {
+        final var book = getBook(index);
+
         books[index] = null;
+        freeIndexes.add(index);
 
         System.out.print(index + "::" + book);
     }
@@ -59,11 +68,11 @@ public class Library {
     }
 
     private int getFirstFreeIndex() {
-        for(int i = 0; i < books.length; i++) {
-            if(books[i] == null) {
-                return i;
-            }
+        if(freeIndexes.isEmpty()) {
+            throw new IllegalArgumentException("There is not free place for book");
         }
-        throw new IllegalArgumentException("There is not free place for book");
+        int index = freeIndexes.first();
+        freeIndexes.remove(index);
+        return index;
     }
 }
